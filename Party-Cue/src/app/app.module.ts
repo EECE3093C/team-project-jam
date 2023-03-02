d314964d9be38f1cca45edd24638af15aca8d6e7
AlbinoDrake238 <spkleeh@gmail.com>
2023-03-01 21:17:53 -0500

    Website with basic master view layout

--- Party-Cue/src/app/app.module.ts
+++ Party-Cue/src/app/app.module.ts
@@ -1,21 +1,41 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
 
+//Angular Material Components
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+
+//Generated Components
+import { AppComponent } from './app.component';
+import { MasterViewComponent } from './master-view/master-view.component';
+import { InviteViewComponent } from './invite-view/invite-view.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterViewComponent,
+    InviteViewComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
-    MatGridListModule
+    MatGridListModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      {path: 'home', component: WelcomePageComponent},
+      {path: 'master', component: MasterViewComponent},
+      {path: 'invited', component: InviteViewComponent},
+      {path: '', redirectTo:'/home', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
